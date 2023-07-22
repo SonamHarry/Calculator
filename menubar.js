558eca916a42996276e13ab69635a293db23fcf4
SonamHarry <sonamharry30@gmail.com>
2023-07-21 18:12:55 -0700

    Submenu is collapsed using esc key and expanded state is conveyed

--- menubar.js
+++ menubar.js
@@ -1,5 +1,4 @@
-  
-  function tabClick(menubar) {
+function tabClick(menubar) {
     let menu1 = document.getElementById("menu1");
     let menu2 = document.getElementById("menu2");
     let menu3 = document.getElementById("menu3");
@@ -10,26 +9,26 @@
     menu1.setAttribute("tabindex", "-1");
     menu2.setAttribute("tabindex", "-1");
     menu3.setAttribute("tabindex", "-1");
-    menu1.setAttribute("aria-selected", "false");
-    menu2.setAttribute("aria-selected", "false");
-    menu3.setAttribute("aria-selected", "false");
+    menu1.setAttribute("aria-expanded", "false");
+    menu2.setAttribute("aria-expanded", "false");
+    menu3.setAttribute("aria-expanded", "false");
     // x.style.display = "none";
     y.style.display = "none";
     z.style.display = "none";
 
     if (menubar === 1) {
       // x.style.display = "block";
-      menu1.setAttribute("aria-selected", "true");
+      menu1.setAttribute("aria-expanded", "true");
       menu1.setAttribute("tabindex", "0");
       menu1.focus();
     } else if (menubar === 2) {
       y.style.display = "block";
-      menu2.setAttribute("aria-selected", "true");
+      menu2.setAttribute("aria-expanded", "true");
       menu2.setAttribute("tabindex", "0");
       menu2.focus();
     } else if (menubar === 3) {
       z.style.display = "block";
-      menu3.setAttribute("aria-selected", "true");
+      menu3.setAttribute("aria-expanded", "true");
       menu3.setAttribute("tabindex", "0");
       menu3.focus();
     }
@@ -50,6 +49,12 @@
       e.target.click();
       Overview.focus();
       Apply.focus();
+    } else if (e.key === "Escape") {
+      e.target.click();
+      document.getElementById('content2').style.display = 'none';
+      menu2.setAttribute('aria-expanded', 'false');
+      document.getElementById('content3').style.display = 'none';
+      menu3.setAttribute('aria-expanded', 'false');
     }
   });
 
@@ -62,32 +67,32 @@
       const previousElement = currentOption.previousElementSibling;
       if (previousElement) {
         previousElement.querySelector('a').focus();
-        previousElement.setAttribute("aria-selected", "true");
+        previousElement.setAttribute("aria-expanded", "true");
         previousElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       } else {
         const lastElement = currentOption.parentElement.lastElementChild;
         lastElement.querySelector('a').focus();
-        lastElement.setAttribute("aria-selected", "true");
+        lastElement.setAttribute("aria-expanded", "true");
         lastElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       }
     } else if (e.key === "ArrowDown") {
       const nextElement = currentOption.nextElementSibling;
       if (nextElement) {
          nextElement.querySelector('a').focus();
-        nextElement.setAttribute("aria-selected", "true");
+        nextElement.setAttribute("aria-expanded", "true");
         nextElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       } else {
         const firstElement = currentOption.parentElement.firstElementChild;
         firstElement.querySelector('a').focus();
-        firstElement.setAttribute("aria-selected", "true");
+        firstElement.setAttribute("aria-expanded", "true");
         firstElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       }
     }
@@ -99,32 +104,32 @@
       const previousElement = currentOption.previousElementSibling;
       if (previousElement) {
         previousElement.querySelector('a').focus();
-        previousElement.setAttribute("aria-selected", "true");
+        previousElement.setAttribute("aria-expanded", "true");
         previousElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       } else {
         const lastElement = currentOption.parentElement.lastElementChild;
          lastElement.querySelector('a').focus();
-        lastElement.setAttribute("aria-selected", "true");
+        lastElement.setAttribute("aria-expanded", "true");
         lastElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       }
     } else if (e.key === "ArrowDown") {
       const nextElement = currentOption.nextElementSibling;
       if (nextElement) {
        nextElement.querySelector('a').focus();
-        nextElement.setAttribute("aria-selected", "true");
+        nextElement.setAttribute("aria-expanded", "true");
         nextElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       } else {
         const firstElement = currentOption.parentElement.firstElementChild;
         firstElement.querySelector('a').focus();
-        firstElement.setAttribute("aria-selected", "true");
+        firstElement.setAttribute("aria-expanded", "true");
         firstElement.setAttribute("tabindex", "0");
-        currentOption.setAttribute("aria-selected", "false");
+        currentOption.setAttribute("aria-expanded", "false");
         currentOption.setAttribute("tabindex", "-1");
       }
     }
