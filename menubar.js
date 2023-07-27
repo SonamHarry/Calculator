09fd6bb7b7c402531cee91f0c6a1f4b0fbf70141
SonamHarry <sonamharry30@gmail.com>
2023-07-27 09:22:37 -0700

    Menubar

--- menubar.js
+++ menubar.js
@@ -1,40 +1,18 @@
-function tabClick(menubar) {
-  let menu1 = document.getElementById("menu1");
-  let menu2 = document.getElementById("menu2");
-  let menu3 = document.getElementById("menu3");
-  let x = document.getElementById("content1");
-  let y = document.getElementById("content2");
-  let z = document.getElementById("content3");
-
-  menu1.setAttribute("tabindex", "-1");
-  menu2.setAttribute("tabindex", "-1");
-  menu3.setAttribute("tabindex", "-1");
-  // menu1.setAttribute("aria-expanded", "false");
-  menu2.setAttribute("aria-expanded", "false");
-  menu3.setAttribute("aria-expanded", "false");
-  // x.style.display = "none";
-  y.style.display = "none";
-  z.style.display = "none";
-
-  if (menubar === 1) {
-    // x.style.display = "block";
-    // menu1.setAttribute("aria-expanded", "true");
-    menu1.setAttribute("tabindex", "0");
-    menu1.focus();
-  } else if (menubar === 2) {
-    y.style.display = "block";
-    menu2.setAttribute("aria-expanded", "true");
-    menu2.setAttribute("tabindex", "0");
-    menu2.focus();
-  } else if (menubar === 3) {
-    z.style.display = "block";
-    menu3.setAttribute("aria-expanded", "true");
-    menu3.setAttribute("tabindex", "0");
-    menu3.focus();
+// Tutorial services
+function togglemenu(menu) {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+    menu.setAttribute("aria-expanded", "false");
+    menu.setAttribute("tabindex", "-1");
+  } else {
+    menu.style.display = "block";
+    menu.setAttribute("aria-expanded", "true");
+    menu.setAttribute("tabindex", "0");
+    menu.querySelector("a").focus();
   }
+
 }
 const menu = document.getElementById("menu");
-
 menu.addEventListener("keydown", e => {
   if (e.key === "ArrowRight") {
     const nextMenuItem = e.target.parentElement.nextElementSibling;
@@ -54,8 +32,9 @@ menu.addEventListener("keydown", e => {
     }
   } else if (e.key === "Enter" || e.key === " ") {
     e.target.click();
-    Overview.focus();
-    Apply.focus();
+    // menu4.focus();
+
+    // menu5.focus();
   }
   else if (e.key === "ArrowDown") {
     const currentMenuItem = e.target.parentElement;
@@ -117,14 +96,27 @@ content2.addEventListener("keydown", function (e) {
       currentOption.setAttribute("aria-expanded", "false");
       currentOption.setAttribute("tabindex", "-1");
     }
-  } else if (e.key === "ArrowRight") {
-    content2.style.display = 'none';
-    menu2.setAttribute('aria-expanded', 'false');
-    menu3.focus();
-
   }
+  else if (e.key === "ArrowRight") {
+   toggleSubmenu(content4);
+  
+    // content2.style.display = 'none';
+    // menu2.setAttribute('aria-expanded', 'false');
+    // menu3.focus();
+  }
+}
+);
+  
 
-});
+  
+
+//    else if (e.key === "ArrowRight") {
+//     toggleSubmenu(content4);
+//     // content2.style.display = 'none';
+//     // menu2.setAttribute('aria-expanded', 'false');
+//     // menu3.focus();
+//   }
+// });
 
 content3.addEventListener("keydown", function (e) {
   const currentOption = e.target.parentElement;
@@ -161,12 +153,121 @@ content3.addEventListener("keydown", function (e) {
       currentOption.setAttribute("aria-expanded", "false");
       currentOption.setAttribute("tabindex", "-1");
     }
-  } else if (e.key === "ArrowLeft") {
-    content3.style.display = 'none';
-    menu3.setAttribute('aria-expanded', 'false');
-    menu2.focus();
+  }
+  // } else if (e.key === "ArrowLeft") {
+    else if (e.key === "ArrowRight") {
+      toggleSubmenu(content5);
+      const firstSubmenuItem = content5.querySelector("a");
+    firstSubmenuItem.focus();
+    // content3.style.display = 'none';
+    // menu3.setAttribute('aria-expanded', 'false');
+    // menu2.focus();
   }
 });
+
+
+const content4 = document.getElementById("content4");
+const content5 = document.getElementById("content5");
+
+content4.addEventListener("keydown", function (e) {
+  const currentOption = e.target.parentElement;
+  if (e.key === "ArrowUp") {
+    const previousElement = currentOption.previousElementSibling;
+    if (previousElement) {
+      previousElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+    } else {
+      const lastElement = currentOption.parentElement.lastElementChild;
+      lastElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+
+    }
+  } else if (e.key === "ArrowDown") {
+    const nextElement = currentOption.nextElementSibling;
+    if (nextElement) {
+      nextElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+    } else {
+      const firstElement = currentOption.parentElement.firstElementChild;
+      firstElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+    }
+  
+  } else if (e.key === "ArrowRight") {
+    // Show the submenu (HTML) and set focus on the first submenu item
+    toggleSubmenu(content4);
+    const firstSubmenuItem = content4.querySelector("a");
+    firstSubmenuItem.focus();
+  } else if (e.key === "ArrowLeft") {
+    // Hide the submenu (HTML) and set focus back to the "Tutorials" menuitem (menu2)
+    content4.style.display = 'none';
+    menu4.setAttribute('aria-expanded', 'false');
+    menu4.focus();
+  }
+});
+
+content5.addEventListener("keydown", function (e) {
+  const currentOption = e.target.parentElement;
+  if (e.key === "ArrowUp") {
+    const previousElement = currentOption.previousElementSibling;
+    if (previousElement) {
+      previousElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+    } else {
+      const lastElement = currentOption.parentElement.lastElementChild;
+      lastElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+
+    }
+  } else if (e.key === "ArrowDown") {
+    const nextElement = currentOption.nextElementSibling;
+    if (nextElement) {
+      nextElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+    } else {
+      const firstElement = currentOption.parentElement.firstElementChild;
+      firstElement.querySelector('a').focus();
+      currentOption.setAttribute("aria-expanded", "true");
+      currentOption.setAttribute("tabindex", "0");
+      currentOption.setAttribute("aria-expanded", "false");
+      currentOption.setAttribute("tabindex", "-1");
+    }
+  
+  } else if (e.key === "ArrowRight") {
+    // Show the submenu (Quizzes) and set focus on the first submenu item
+    toggleSubmenu(content5);
+    const firstSubmenuItem = content5.querySelector("a");
+    firstSubmenuItem.focus();
+  } else if (e.key === "ArrowLeft") {
+    // Hide the submenu (Quizzes) and set focus back to the "Services" menuitem (menu3)
+    content5.style.display = 'none';
+    menu5.setAttribute('aria-expanded', 'false');
+    menu5.focus();
+  }
+});
+
+
 
 const homeLink = document.querySelector(".home a");
 
@@ -179,10 +280,24 @@ homeLink.addEventListener("focus", () => {
     aboutUsMenu.setAttribute("aria-expanded", "false");
     aboutUsMenu.setAttribute("tabindex", "-1");
   }
-
   if (admissionsMenu) {
     admissionsMenu.style.display = "none";
     admissionsMenu.setAttribute("aria-expanded", "false");
     admissionsMenu.setAttribute("tabindex", "-1");
   }
 });
+
+// HTML Quizzes
+function toggleSubmenu(submenu) {
+  if (submenu.style.display === "block") {
+    submenu.style.display = "none";
+    submenu.setAttribute("aria-expanded", "false");
+    submenu.setAttribute("tabindex", "-1");
+  } else {
+    submenu.style.display = "block";
+    submenu.setAttribute("aria-expanded", "true");
+    submenu.setAttribute("tabindex", "0");
+    submenu.querySelector("a").focus();
+  }
+}
+
